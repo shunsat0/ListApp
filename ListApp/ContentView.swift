37e1db2a59a7b7251cbaf5f69ac52fd8bc1f5982
shunsat0 <ellegardenno8@gmail.com>
2024-08-05 06:53:55 +0900

    Initial Commit

---
+++ ListApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListApp
+//
+//  Created by SHUN SATO on 2024/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by SHUN SATO on 2024/08/05.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
